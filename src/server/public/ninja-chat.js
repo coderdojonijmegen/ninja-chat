af8748374eed9654f429c97f1e9c3bd3abd0a15a
Rik Bosch <rik@luminext.eu>
2018-11-01 11:34:54 +0100

    Voorbeeld app JS gelijk getrokken met client boilerplate.

--- src/server/public/ninja-chat.js
+++ src/server/public/ninja-chat.js
@@ -3,66 +3,111 @@ var serverIpAdresEnPoort = "http://127.0.0.1:3000";
 
 var socket = io(serverIpAdresEnPoort, { forceNew: true });
 
+/**
+ * Geeft de gekozen naam door aan de server.
+ * 
+ * @param {jQuery.Event} input 
+ */
 function zetNaam(input) {
-    var naam = input.target.value
-    socket.emit('zetNaam', naam)
+  var naam = input.target.value
+  socket.emit('zetNaam', naam)
 }
 
+/**
+ * Geeft het gekozen kanaal door aan de server.
+ * 
+ * @param {jQuery.Event} input 
+ */
 function zetKanaal(input) {
-    var kanaal = input.target.value
-    socket.emit('zetKanaal', kanaal)
+  var kanaal = input.target.value
+  socket.emit('zetKanaal', kanaal)
 }
 
+/**
+ * Stuurt een bericht naar de server en maakt de bericht-input weer leeg.
+ */
 function stuurBericht() {
-    var berichtInput = $("#berichtInput")
-    var bericht = berichtInput.val()
-    if (bericht != "") {
-      socket.emit('maakBericht', bericht)
-      berichtInput.val("")
-    }
+  var berichtInput = $("#berichtInput")
+  var bericht = berichtInput.val()
+  if (bericht != "") {
+    socket.emit('maakBericht', bericht)
+    berichtInput.val("")
+  }
 }
 
+/**
+ * Als op enter wordt gedrukt, stuur bericht.
+ * 
+ * @param {Event} event 
+ */
 function checkEnter(event) {
-    if (event.which === 13) {
-        stuurBericht()
-    }
+  if (event.which === 13) {
+      stuurBericht()
+  }
 }
 
+/**
+ * Dit zorgt ervoor dat de chat-naam zichtbaar is.
+ * 
+ * @param {string} naam 
+ */
 function toonNaam(naam) {
-    $("#naamInput").val(naam)
+  $("#naamInput").val(naam)
 }
 
+/**
+ * Dit zorgt ervoor dat het huidige kanaal zichtbaar is.
+ * 
+ * @param {number} kanaal 
+ */
 function toonKanaal(kanaal) {
-    $("#kanaal").val(kanaal)
+  $("#kanaalInput").val(kanaal)
 }
 
+/**
+ * Dit zorgt ervoor dat een bericht op het scherm komt.
+ * 
+ * @param {object} bericht 
+ */
 function toonBericht(bericht) {
-    var div = $('<div>').html(berichtNaarHtml(bericht))
-    $("main").append(div)
+  var div = $('<div>').html(berichtNaarHtml(bericht))
+  $(".berichten").append(div)
 }
 
+/**
+ * Hier wordt een bericht vertaald naar mooie HTML.
+ * 
+ * @param {object} bericht 
+ */
 function berichtNaarHtml(bericht) {
-    var gebruiker = $("#naamInput").val()
-    var class_naam = "bericht"
+  var gebruiker = $("#naamInput").val()
+  var class_naam = "bericht"
+  
+  if (gebruiker === bericht.gebruiker) {
+      class_naam = "eigen bericht"
+  }
 
-    if (gebruiker === bericht.gebruiker) {
-        class_naam = "eigen bericht"
-    }
-
-    return '<p class="' + class_naam + '"><i>'
-        + bericht.gebruiker + ' ' + bericht.tijdstip
-        + '</i><br><span>' + bericht.tekst + '</span></p>'
+  return `
+    <p class="${class_naam}">
+      <i>${bericht.gebruiker} ${bericht.tijdstip}</i>
+      <br>
+      <span>${bericht.tekst}</span>
+    </p>`
 }
 
-function begin() {
-    $("#naamInput").change(zetNaam)
-    $("#kanaal").change(zetKanaal)
-    $(".stuurBericht").click(stuurBericht)
-    $("#berichtInput").keypress(checkEnter)
 
-    socket.on('krijgKanaal', toonKanaal)
-    socket.on('krijgNaam', toonNaam)
-    socket.on('krijgBericht', toonBericht)
+/**
+ * Dit is waar alle gebeurtenissen worden ingesteld.
+ */
+function begin() {
+  $("#naamInput").change(zetNaam)
+  $("#kanaalInput").change(zetKanaal)
+  $(".stuurBericht").click(stuurBericht)
+  $("#berichtInput").keypress(checkEnter)
+
+  socket.on('krijgKanaal', toonKanaal)
+  socket.on('krijgNaam', toonNaam)
+  socket.on('krijgBericht', toonBericht)
 }
 
 $(begin)
